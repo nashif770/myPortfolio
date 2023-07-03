7aaf556bd3ef3ad1eb370d885f98a8c74b6bbfba
Ahmed Nashif <a.nashif7@gmail.com>
2023-07-03 19:56:24 +0600

    styling

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -18,14 +18,11 @@ const ContactMe = () => {
   };
 
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero bg-slate-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={sendEmail} ref={form} className="card-body">
+        <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={sendEmail} ref={form} className="card-body flex flex-row">
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
               <input
                 type="text"
                 name="userEmail"
--- src/Pages/Home/Contact.jsx
+++ src/Pages/Home/Contact.jsx
@@ -5,7 +5,7 @@ import ContactMe from "../../Components/ContactMe";
 const Contact = () => {
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col pb-12">
       <Header heading={"Ready to Work with Me?"}></Header>
       <div
         className="flex flex-col align-middle text-center border shadow-lg rounded-lg m-3 mt-20"
@@ -15,13 +15,14 @@ const Contact = () => {
           <p className="text-4xl font-bold uppercase">Say Hello!</p>
         </div>
         <h3 className="text-4xl m-6">Ahmed Nashif</h3>
-        <div className="mb-12 flex justify-center">
+        <div className="mb-3 flex justify-center">
           <p className="m-3">a.nashif7@gmail.com</p>
           <p className="m-3">+880-1986854470</p>
         </div>
-      </div>
-      
+        <div className="divider">Or</div>
+        <p>Get my resume in mail</p>
       <ContactMe></ContactMe>
+      </div>
     </div>
   );
 };
